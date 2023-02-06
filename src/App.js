861e641b0c022a3148b220ef2555b333251c1539
Anant Singhal <anantsinghal47@gmail.com>
2023-02-06 09:13:43 +0530

    carousel added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
 import AgridTable from './Components/AgridTable'
-import ButtonExampleEmphasis from './Components/Buttons/buttons';
+import ButtonExampleEmphasis from './Components/Buttons/buttons'; 
 import MainForm from './Components/Forms/MainForm';
 import FormExampleFieldControlId from './Components/Forms/MainForm';
 import MenuExampleSecondary from './Components/NavBar/Menu';
 import TabExampleColoredInverted from './Components/tabbar';
 import Header from './Header.js';
 import FooterBottom from './FooterBottom.js';
+import Carousel , {CarouselItem} from './Carousel';
 
 function App() {
   return (
@@ -17,6 +18,11 @@ function App() {
     {/* <TabExampleColoredInverted/> */}
     {/* <MenuExampleSecondary/> */}
     <Header/>
+    <Carousel>
+      <CarouselItem>Item1</CarouselItem>
+      <CarouselItem>Item2</CarouselItem>
+      <CarouselItem>Item3</CarouselItem>
+    </Carousel>
     <MainForm />  
     <AgridTable />
     <FooterBottom/>
---
+++ src/Carousel.js
@@ -0,0 +1,44 @@
+
+//https://medium.com/tinyso/how-to-create-the-responsive-and-swipeable-carousel-slider-component-in-react-99f433364aa0
+import React, { useState } from 'react'
+import './Carousel.css'
+
+export const CarouselItem = ({children , width}) => {
+  return (
+    <div className='carousel-item' style={{width: width }}>
+      {children}
+    </div>
+  );
+};
+
+
+
+const  Carousel = ({ children }) => {
+
+  const [activeIndex , setActiveIndex] = useState(0);
+  const updateIndex = (newIndex) =>{
+    if(newIndex < 0)
+    {
+      newIndex = 0;
+    }
+    else if(newIndex >= React.Children.count(children)){
+      newIndex = React.Children.count(children)-1;
+    }
+    setActiveIndex(newIndex);
+  };
+
+
+  return (
+    <div className='carousel'>
+      <div className='inner' style={{ transform: `translateX(-${activeIndex*100}%)` }}>
+
+        {React.Children.map(children, (child, index) => {
+          return React.cloneElement(child, { width: "100%" });
+        })}
+      </div>
+
+    </div>
+  )
+}
+
+export default Carousel
