a10042c15f96a23cb4b4d98475c7125ba27aabbf
Anant Singhal <76203573+anantsinghal47@users.noreply.github.com>
2023-05-12 21:53:16 +0530

    Create datepicker.js

---
+++ datepicker.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+
+function App() {
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [showDatePicker, setShowDatePicker] = useState(false);
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+    setShowDatePicker(false);
+  };
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#007bff',
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div>
+        <Button onClick={() => setShowDatePicker(true)}>Choose Date</Button>
+        {showDatePicker && (
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <DatePicker
+              value={selectedDate}
+              onChange={handleDateChange}
+              renderInput={(params) => <TextField {...params} />}
+            />
+          </LocalizationProvider>
+        )}
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
